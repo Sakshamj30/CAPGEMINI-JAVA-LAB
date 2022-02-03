b525642d6bd4a7e456c8f253227ce02a13cbb206
Saksham Jauhari <98949528+Sakshamj30@users.noreply.github.com>
2022-02-03 16:25:09 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,16 @@
+package com.capgExcersies;
+
+public class Program2 
+	{
+		public static void main(String args[])
+		{
+			int x;
+			for (x = 0; x < 4; x++)
+			{
+				System.out.println(x);
+			}
+
+		System.out.println(x);
+		}
+	}
+
